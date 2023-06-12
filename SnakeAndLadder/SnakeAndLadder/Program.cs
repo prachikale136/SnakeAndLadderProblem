f354a60d5b3ae85f8e2d60cb8c3e2d4331d70daa
Prachi <prachikale136@gmail.com>
2023-06-12 22:04:51 +0530

    [PRachi] Add: Welcome massage

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and LAdddr problem");
+        }
+    }
+}
