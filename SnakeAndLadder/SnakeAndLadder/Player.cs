432d8c3ac9d8261bfd08d652eeeacc44963d7695
Prachi <prachikale136@gmail.com>
2023-06-12 22:09:31 +0530

    [Prachi] Add:game played with Single Player

---
+++ SnakeAndLadder/SnakeAndLadder/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Player
+    {
+        public int StartPoint = 0;
+        public void StartPlaying()
+        {
+            Console.WriteLine("Game started");
+            Console.WriteLine("you are at " + StartPoint);
+        }
+    }
+}
